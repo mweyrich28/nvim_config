2b87c98b6395ad09e86ed4117c0dc0af582fc62a
Malte Weyrich <malte.weyrich@hotmail.de>
2024-03-20 13:41:06 +0100

    Fixed backlinks

--- lua/user/vimwiki_backlinks.lua
+++ lua/user/vimwiki_backlinks.lua
@@ -3,8 +3,8 @@ local telescope = require("telescope.builtin")
 local function search_backlinks()
     local current_file = vim.fn.expand('%:t') -- Get the current file name
     -- Remove .* extension
-    current_file = current_file:gsub("%..*", "")
-    local search_pattern = "\\[*\\]\\(" .. current_file -- Create a search pattern
+    current_file = current_file:gsub(".md", "")
+    local search_pattern = "\\[*\\]\\(.*" .. current_file -- Create a search pattern
 
     telescope.live_grep({
         prompt_title = "Wiki Backlinks",

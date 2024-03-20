d2cffc61cb7761de753e3e25356ce628d9d94638
Malte Weyrich <malte.weyrich@hotmail.de>
2024-03-20 13:35:16 +0100

    Fixed relative paths for vimwiki link

--- lua/user/vimwiki_link.lua
+++ lua/user/vimwiki_link.lua
@@ -4,8 +4,16 @@ local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 
+function split(str)
+    local parts = {}
+    for part in string.gmatch(str, "/") do
+        table.insert(parts, part)
+    end
+    return parts
+end
+
 local vimwiki_link = function()
-    local opts =  require("telescope.themes").get_dropdown { prompt_title = "Vimwiki Link" }
+    local opts = require("telescope.themes").get_dropdown { prompt_title = "Vimwiki Link" }
 
     local results = vim.fn.systemlist("find /home/malte/documents/vimwiki_para -type f -name '*.md'")
     local processed_results = {}
@@ -27,9 +35,32 @@ local vimwiki_link = function()
                 actions.close(prompt_bufnr)
                 local markdown_abs = selection.value
                 local markdown_name = markdown_abs.match(markdown_abs, "[^/]+$")
+
+                local current_file_path = vim.fn.expand("%:p")            -- Get the full path of the current file
+                local current_file_dir = current_file_path:match("(.*/)") -- Extract the directory part
+
+                -- split paths
+                local root_depth = 5
+                local current_file_path_table = split(current_file_path)
+                local relative_path = ""
+
+                local curr_depht = 0
+                for i, part in ipairs(current_file_path_table) do
+                    curr_depht = curr_depht + 1
+                end
+
+
+                -- Count the number of directories to go up
+                local current_dir_level = current_file_dir:gsub("[^/]*", "")
+                for i=1, curr_depht - root_depth,1 do
+                    relative_path = relative_path .. "../"
+                end
+
+                -- Append the path to the markdown file
+                relative_path = relative_path .. markdown_abs
                 -- local markdown_file = string.match(markdown_abs, "vimwiki_para/(.*)") -- get only the part after vimwiki_para/
                 -- split markdown_file at . and take the first part
-                local wiki_link = "[" .. string.gsub(markdown_name, ".md", "") .. "]" .. "(" .. markdown_abs .. ")"
+                local wiki_link = "[" .. string.gsub(markdown_name, ".md", "") .. "]" .. "(" .. relative_path .. ")"
                 -- put the selected string under the cursor
                 vim.api.nvim_put({ wiki_link }, "", true, true)
             end)
